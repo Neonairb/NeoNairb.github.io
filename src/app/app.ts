dbeb8d2a49056b2f2c01d4e07317f38939b1ecbc
NeoNairb <brajai@hotmail.com>
2025-09-19 20:16:20 -0600

    add waiting page

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,13 +4,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
+  styleUrls: ['./app.css'],
   imports: [
     CommonModule
-  ],
-  styleUrls: ['./app.css']
+  ]
 })
 export class App {
   isVisible = false;
+
+  // Nuevo: mensaje especial según la fecha
+  showGiftMessage: boolean;
+
+  constructor() {
+    const today = new Date();
+    const month = today.getMonth() + 1; // Enero=0
+    const day = today.getDate();
+
+    // month = 9; // Para pruebas
+    // day = 21; // Para pruebas
+
+    // Solo mostrar regalo si es 21 de septiembre
+    this.showGiftMessage = (month === 9 && day === 21);
+
+    console.log(`showGiftMessage: ${this.showGiftMessage}`);
+  }
+
 
   showMessage() {
     this.isVisible = true;
